ea6661b08b78dae7298aa0151568ef37fb8a2ef9
Eurecha <eurecha@brainmatics.com>
2019-07-29 23:24:44 +0700

    [FIX] Perbaikan api search comic ciayo

--- ciayo/getChapters.php
+++ ciayo/getChapters.php
@@ -1,23 +1,21 @@
 <?php
 function ciayoSearch($judul=null)
 {
-	$judul = (isset($judul) ? $judul : 'their story');
-	$judul = preg_replace('/\s+/', '+', $judul);
-	$url = "https://www.google.com/search?q=site:ciayo.com+$judul&oq=site:ciayo.com+$judul";
+	$judul = (isset($judul) && $judul != null ? $judul : 'their story');
+	$url = "https://id09wekzii-dsn.algolia.net/1/indexes/*/queries?x-algolia-application-id=ID09WEKZII&x-algolia-api-key=e023a8b3946ae4953b0c545c0d853c80";
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS,
+            '{"requests":[{"indexName":"comics_id","params":"query='.$judul.'&page=0"}]}');
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	$html = curl_exec($ch);
 
 	curl_close($ch);
 
-	@preg_match_all('/<div id="ires"><ol><div class="g">(.+?)<\/div>/m', $html, $links);
-	@preg_match('/<cite>(.+?)<\/cite>/m', $html, $cite);
-	@$cite = strip_tags($cite[1]);
-	@$data = explode('/', $cite);
-	@$alias = end($data);
+	$alias = json_decode($html)->results[0]->hits[0]->alias;
 
 	return $alias;
 }
